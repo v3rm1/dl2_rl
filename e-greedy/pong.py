58b6c7478dba4fa60130c52a06415e290c7caf41
Varun Ravi Varma <varunravivarma@gmail.com>
2019-06-12 13:59:16 +0200

    Updated e-greedy pong

--- e-greedy/pong.py
+++ e-greedy/pong.py
@@ -19,7 +19,7 @@ class Game:
     """
     Defining a class for initializing and running the game
     """
-    def __init__(self, sess, model, env, memory, max_epsilon, min_epsilon, decay, render_game=True):
+    def __init__(self, sess, model, env, memory, max_epsilon, min_epsilon, decay, render_game=False):
         self._sess = sess
         self._model = model
         self._env = env
@@ -44,24 +44,29 @@ class Game:
 
             action = self._choose_action(state)
             next_state, reward, done, info = self._env.step(action)
-            if next_state[0] >= 0.1:
-                reward += 10
-            elif next_state[0] >= 0.25:
-                reward += 20
-            elif next_state[0] >= 0.5:
-                reward += 100
+            print("Current action: {}\nCurrent Reward: {}".format(action, reward))
+            # print(next_state[0])
+            # if next_state[0] >= 0.1:
+            #     reward += 10
+            # elif next_state[0] >= 0.25:
+            #     reward += 20
+            # elif next_state[0] >= 0.5:
+            #     reward += 100
 
-            if next_state[0] > max_x:
-                max_x = next_state[0]
+            # if next_state[0] > max_x:
+            #     max_x = next_state[0]
             
             if done:
                 next_state = None
             
             self._memory.add_sample((state, action, reward, next_state))
-            self._replay()
+            # self._replay()
 
             self._steps += 1
             self._epsilon = MIN_EPSILON + (MAX_EPSILON - MIN_EPSILON) * math.exp(-LAMBDA * self._steps)
+            print("Updated epsilon: {}".format(self._epsilon))
+            if self._steps > 50:
+                self._replay()
 
             state = next_state
             total_reward += reward
@@ -73,18 +78,30 @@ class Game:
             print("Step {}:\n Total Reward: {}, Epsilon: {}".format(self._steps, total_reward, self._epsilon))
             
     def _choose_action(self, state):
-        if random.random() > self._epsilon:
+        i = random.random()
+        print("Random.random: ", i)
+        if i > self._epsilon:
             return random.randint(0, self._model.action_count - 1)
         else:
             return np.argmax(self._model.predict_one(state, self._sess))
         
     
-    def replay(self):
+    def _replay(self):
         batch = self._memory.sample(self._model.batch_size)
-        states = np.array(batch_val[0] for batch_val in batch)
-        next_states = np.array([(np.zeros(self._model.state_count) if batch_val[3] is None else batch_val[3] for batch_val in batch)])
-        q_value = self._model.predict_batch(states, self._sess)
-        q_value_pred = self._model.predict_batch(next_states, self._sess)
+        states = []
+        next_states = []
+        for i in range(self._model.batch_size):
+            states.append(batch[i][0])
+            if batch[i][3] is None:
+                next_states.append(np.zeros(self._model.state_count))
+            else:
+                next_states.append(batch[i][3])
+        
+        # states = np.array(batch_val[0][0] for batch_val in batch)
+        # states = self._memory.sample(self._model.batch_size)[0][0]
+        # next_states = np.array([(np.zeros(self._model.state_count) if batch_val[3] is None else batch_val[3] for batch_val in batch)])
+        q_value = self._model.predict_batch(np.reshape(states, (len(batch), self._model.state_count)), self._sess)
+        q_value_pred = self._model.predict_batch(np.reshape(next_states, (len(batch), self._model.state_count)), self._sess)
         x = np.zeros((len(batch), self._model.state_count))
         y = np.zeros((len(batch), self._model.action_count))
         for i, b in enumerate(batch):
@@ -94,8 +111,10 @@ class Game:
                 current_q_value[action] = reward
             else:
                 current_q_value[action] = reward + GAMMA * np.amax(q_value_pred[i])
+                print("Q Value Predicted: ", q_value_pred[i])
+                print("Current Q Value Action: ", current_q_value[action])
             
-            x[i] = state
+            x[i] = np.reshape(state, (1, self._model.state_count))
             y[i] = current_q_value
         
         self._model.train_batch(self._sess, x, y)
@@ -111,11 +130,11 @@ class Game:
 
 
 if __name__=="__main__":
-    env_name = "MountainCar-v0"
+    env_name = "PongNoFrameskip-v4"
     env = gym.make(env_name)
 
-    num_states = env.env.observation_space.shape[0]
-    num_actions = env.env.action_space.n
+    num_states = (210*160*3)
+    num_actions = env.action_space.n
 
     model = model.Model(num_states, num_actions, BATCH_SIZE)
     mem = memory.Memory(50000)
