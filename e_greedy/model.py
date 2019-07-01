59a92ef492de00d544d8e2fc1ca9ab2f8182a4e6
Varun Ravi Varma <varunravivarma@gmail.com>
2019-07-01 16:57:08 +0200

    Updated e-greedy

--- e_greedy/model.py
+++ e_greedy/model.py
@@ -1,59 +1,183 @@
 import tensorflow as tf
-import numpy as np
+import cv2 #read in pixel data
+import pong #our class
+import numpy as np #math
+import random #random 
+from collections import deque #queue data structure. fast appends. and pops. replay memory
 
-# TODO: Add method comments and verify if the neural network structure is valid
-class Model:
-    """
-    Defining the model class
-    """
-    def __init__(self, state_count, action_count, batch_size):
-        self._state_count = state_count
-        self._action_count = action_count
-        self._batch_size = batch_size
 
-        # Placeholders for state and action space
-        self._states = None
-        self._actions = None
-        
-        # Output operations
-        self._logits = None
-        self._optimizer = None
-        self._var_init = None
 
-        self._define_model()
+#hyper params
+ACTIONS = 3 #up,down, stay
+#define our learning rate
+GAMMA = 0.99
+#for updating our gradient or training over time
+INITIAL_EPSILON = 1.0
+FINAL_EPSILON = 0.05
+#how many frames to anneal epsilon
+EXPLORE = 500000 
+OBSERVE = 50000
+#store our experiences, the size of it
+REPLAY_MEMORY = 500000
+#batch size to train on
+BATCH = 100
 
-    def _define_model(self):
-        self._states = tf.placeholder(shape=[None, self._state_count], dtype=tf.float32)
-        self._q_value = tf.placeholder(shape=[None, self._action_count], dtype=tf.float32)
-        fc1 = tf.layers.dense(self._states, 64, activation=tf.nn.relu)
-        fc2 = tf.layers.dense(fc1, 64, activation=tf.nn.relu)
-        self._logits = tf.layers.dense(fc2, self._action_count)
-        loss = tf.losses.mean_squared_error(self._q_value, self._logits)
-        self._optimizer = tf.train.AdamOptimizer().minimize(loss)
-        self._var_init = tf.global_variables_initializer()
+#create tensorflow graph
+def createGraph():
 
-    def predict_one(self, state, sess):
-        return sess.run(self._logits, feed_dict={self._states: state.reshape(1, self.state_count)})
+    #first convolutional layer. bias vector
+    #creates an empty tensor with all elements set to zero with a shape
+    W_conv1 = tf.Variable(tf.zeros([8, 8, 4, 32]))
+    b_conv1 = tf.Variable(tf.zeros([32]))
 
-    def predict_batch(self, states, sess):
-        return sess.run(self._logits, feed_dict={self._states: states})
+    W_conv2 = tf.Variable(tf.zeros([4, 4, 32, 64]))
+    b_conv2 = tf.Variable(tf.zeros([64]))
 
-    def train_batch(self, sess, x_batch, y_batch):
-        sess.run(self._optimizer, feed_dict={self._states: x_batch, self._q_value: y_batch}) 
+    W_conv3 = tf.Variable(tf.zeros([3, 3, 64, 64]))
+    b_conv3 = tf.Variable(tf.zeros([64]))
 
+    W_fc4 = tf.Variable(tf.zeros([3136, 784]))
+    b_fc4 = tf.Variable(tf.zeros([784]))
+
+    W_fc5 = tf.Variable(tf.zeros([784, ACTIONS]))
+    b_fc5 = tf.Variable(tf.zeros([ACTIONS]))
+
+    #input for pixel data
+    s = tf.placeholder("float", [None, 84, 84, 4])
+
+
+    #Computes rectified linear unit activation fucntion on  a 2-D convolution given 4-D input and filter tensors. and 
+    conv1 = tf.nn.leaky_relu(tf.nn.conv2d(s, W_conv1, strides = [1, 4, 4, 1], padding = "VALID") + b_conv1)
+
+    conv2 = tf.nn.leaky_relu(tf.nn.conv2d(conv1, W_conv2, strides = [1, 2, 2, 1], padding = "VALID") + b_conv2)
+
+    conv3 = tf.nn.leaky_relu(tf.nn.conv2d(conv2, W_conv3, strides = [1, 1, 1, 1], padding = "VALID") + b_conv3)
+
+    conv3_flat = tf.reshape(conv3, [-1, 3136])
+
+    fc4 = tf.nn.relu(tf.matmul(conv3_flat, W_fc4) + b_fc4)
     
-    @property
-    def state_count(self):
-        return self._state_count
+    fc5 = tf.matmul(fc4, W_fc5) + b_fc5
 
-    @property
-    def action_count(self):
-        return self._action_count
+    return s, fc5
 
-    @property
-    def batch_size(self):
-        return self._batch_size
 
-    @property
-    def var_init(self):
-        return self._var_init
+#deep q network. feed in pixel data to graph session 
+def trainGraph(inp, out, sess):
+
+    #to calculate the argmax, we multiply the predicted output with a vector with one value 1 and rest as 0
+    argmax = tf.placeholder("float", [None, ACTIONS]) 
+    gt = tf.placeholder("float", [None]) #ground truth
+
+    #action
+    action = tf.reduce_sum(tf.multiply(out, argmax), reduction_indices = 1)
+    #cost function we will reduce through backpropagation
+    cost = tf.reduce_mean(tf.square(action - gt))
+    #optimization fucntion to reduce our minimize our cost function 
+    train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
+
+    #initialize our game
+    game = pong.PongGame()
+    
+    
+    #create a queue for experience replay to store policies
+    D = deque()
+
+    #intial frame
+    frame = game.getPresentFrame()
+    #convert rgb to gray scale for processing
+    frame = cv2.cvtColor(cv2.resize(frame, (84, 84)), cv2.COLOR_BGR2GRAY)
+    #binary colors, black or white
+    ret, frame = cv2.threshold(frame, 1, 255, cv2.THRESH_BINARY)
+    #stack frames, that is our input tensor
+    inp_t = np.stack((frame, frame, frame, frame), axis = 2)
+
+    #saver
+    saver = tf.train.Saver()
+
+    sess.run(tf.initialize_all_variables())
+
+    t = 0
+    epsilon = INITIAL_EPSILON
+    
+    #training time
+    while(1):
+        #output tensor
+        out_t = out.eval(feed_dict = {inp : [inp_t]})[0]
+        #argmax function
+        argmax_t = np.zeros([ACTIONS])
+
+        #
+        if(random.random() <= epsilon):
+            maxIndex = random.randrange(ACTIONS)
+        else:
+            maxIndex = np.argmax(out_t)
+        argmax_t[maxIndex] = 1
+        
+        if epsilon > FINAL_EPSILON:
+            epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
+
+        #reward tensor if score is positive
+        reward_t, frame = game.getNextFrame(argmax_t)
+        #get frame pixel data
+        frame = cv2.cvtColor(cv2.resize(frame, (84, 84)), cv2.COLOR_BGR2GRAY)
+        ret, frame = cv2.threshold(frame, 1, 255, cv2.THRESH_BINARY)
+        frame = np.reshape(frame, (84, 84, 1))
+        #new input tensor
+        inp_t1 = np.append(frame, inp_t[:, :, 0:3], axis = 2)
+        
+        #add our input tensor, argmax tensor, reward and updated input tensor tos tack of experiences
+        D.append((inp_t, argmax_t, reward_t, inp_t1))
+
+        #if we run out of replay memory, make room
+        if len(D) > REPLAY_MEMORY:
+            D.popleft()
+        
+        #training iteration
+        if t > OBSERVE:
+
+            #get values from our replay memory
+            minibatch = random.sample(D, BATCH)
+        
+            inp_batch = [d[0] for d in minibatch]
+            argmax_batch = [d[1] for d in minibatch]
+            reward_batch = [d[2] for d in minibatch]
+            inp_t1_batch = [d[3] for d in minibatch]
+        
+            gt_batch = []
+            out_batch = out.eval(feed_dict = {inp : inp_t1_batch})
+            
+            #add values to our batch
+            for i in range(0, len(minibatch)):
+                gt_batch.append(reward_batch[i] + GAMMA * np.max(out_batch[i]))
+
+
+
+            #train on that 
+            train_step.run(feed_dict = {
+                           gt : gt_batch,
+                           argmax : argmax_batch,
+                           inp : inp_batch
+                           })
+        
+        #update our input tensor the the next frame
+        inp_t = inp_t1
+        t = t+1
+
+        #print our where wer are after saving where we are
+        if t % 10000 == 0:
+            saver.save(sess, './' + 'pong' + '-dqn', global_step = t)
+
+        print("TIMESTEP", t, "/ EPSILON", epsilon, "/ ACTION", maxIndex, "/ REWARD", reward_t, "/ Q_MAX %e" % np.max(out_t))
+
+
+def main():
+    #create session
+    sess = tf.InteractiveSession()
+    #input layer and output layer by creating graph
+    inp, out = createGraph()
+    #train our graph on input and output with session variables
+    trainGraph(inp, out, sess)
+
+if __name__ == "__main__":
+    main()
--- e_greedy/pong.py
+++ e_greedy/pong.py
@@ -1,161 +1,218 @@
-import gym
-import math
-import memory
-import model
-import random
-import numpy as np
-import tensorflow as tf
-from matplotlib import pylab as plt
+import pygame #helps us make GUI games in python
+import random #help us define which direction the ball will start moving in
 
-MAX_EPSILON = 1
-MIN_EPSILON = 0.01
-LAMBDA = 0.0001
-GAMMA = 0.8
-BATCH_SIZE = 50
+#DQN. CNN reads in pixel data. 
+#reinforcement learning. trial and error.
+#maximize action based on reward
+#agent envrioment loop
+#this is called Q Learning
+#based on just game state. mapping of state to action is policy
+#experience replay. learns from past policies
 
 
-class Game:
-    """
-    Defining a class for initializing and running the game
-    """
-    def __init__(self, sess, model, env, memory, max_epsilon, min_epsilon, decay, render_game=False):
-        self._sess = sess
-        self._model = model
-        self._env = env
-        self._memory = memory
-        self._max_epsilon = max_epsilon
-        self._min_epsilon = min_epsilon
-        self._decay = decay
-        self._steps = 0
-        self._stored_rewards = []
-        self._stored_max_x = []
-        self._epsilon = self._max_epsilon
-        self._render = render_game
+#frame rate per second
+FPS = 60
 
+#size of our window
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 400
+
+#size of our paddle
+PADDLE_WIDTH = 10
+PADDLE_HEIGHT = 60
+#distance from the edge of the window
+PADDLE_BUFFER = 10
+
+#size of our ball
+BALL_WIDTH = 10
+BALL_HEIGHT = 10
+
+#speeds of our paddle and ball
+PADDLE_SPEED = 2
+BALL_X_SPEED = 3
+BALL_Y_SPEED = 2
+
+#RGB colors for our paddle and ball
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+#initialize our screen using width and height vars
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+#Paddle 1 is our learning agent/us
+#paddle 2 is the evil AI
+
+#draw our ball
+def drawBall(ballXPos, ballYPos):
+    #small rectangle, create it
+    ball = pygame.Rect(ballXPos, ballYPos, BALL_WIDTH, BALL_HEIGHT)
+    #draw it
+    pygame.draw.rect(screen, WHITE, ball)
+
+
+def drawPaddle1(paddle1YPos):
+    #crreate it
+    paddle1 = pygame.Rect(PADDLE_BUFFER, paddle1YPos, PADDLE_WIDTH, PADDLE_HEIGHT)
+    #draw it
+    pygame.draw.rect(screen, WHITE, paddle1)
+
+
+def drawPaddle2(paddle2YPos):
+    #create it, opposite side
+    paddle2 = pygame.Rect(WINDOW_WIDTH - PADDLE_BUFFER - PADDLE_WIDTH, paddle2YPos, PADDLE_WIDTH, PADDLE_HEIGHT)
+    #draw it
+    pygame.draw.rect(screen, WHITE, paddle2)
+
+
+#update the ball, using the paddle posistions the balls positions and the balls directions
+def updateBall(paddle1YPos, paddle2YPos, ballXPos, ballYPos, ballXDirection, ballYDirection):
+
+    #update the x and y position
+    ballXPos = ballXPos + ballXDirection * BALL_X_SPEED
+    ballYPos = ballYPos + ballYDirection * BALL_Y_SPEED
+    score = 0
+
+    #checks for a collision, if the ball hits the left side, our learning agent
+    if (
+                        ballXPos <= PADDLE_BUFFER + PADDLE_WIDTH and ballYPos + BALL_HEIGHT >= paddle1YPos and ballYPos - BALL_HEIGHT <= paddle1YPos + PADDLE_HEIGHT):
+        #switches directions
+        ballXDirection = 1
+    #past it
+    elif (ballXPos <= 0):
+        #negative score
+        ballXDirection = 1
+        score = -1
+        return [score, paddle1YPos, paddle2YPos, ballXPos, ballYPos, ballXDirection, ballYDirection]
     
-    def run(self):
-        state = self._env.reset()
-        state = self._preproc(state)
-        total_reward = 0
-        max_x = np.empty(shape=(1), dtype=int)
-        max_x.fill(-100)
-        while True:
-            if self._render:
-                self._env.render()
-
-            action = self._choose_action(state)
-            next_state, reward, done, info = self._env.step(action)
-            next_state = self._preproc(next_state)
-            print("Current action: {}\nCurrent Reward: {}".format(action, reward))
-           
-            max_x = next_state
-                        
-            # if next_state.any() > max_x.any():
-            #     max_x = next_state
-
-            if done:
-                next_state = None
-            
-            self._memory.add_sample((state, action, reward, next_state))
-            self._replay()
-
-            self._steps += 1
-            self._epsilon = MIN_EPSILON + (MAX_EPSILON - MIN_EPSILON) * math.exp(-LAMBDA * self._steps)
-            print("Updated epsilon: {}".format(self._epsilon))
-
-
-            state = next_state
-            total_reward += reward
-
-            if done or state.shape != (6000,) :
-                self._stored_rewards.append(total_reward)
-                self._stored_max_x.append(max_x)
-
-            
-            print("Step {}:\n Total Reward: {}, Epsilon: {}".format(self._steps, total_reward, self._epsilon))
-            
-    def _choose_action(self, state):
-        i = random.random()
-        print("Random.random: ", i)
-        if state is not None:
-            if i < self._epsilon:
-                return random.randint(0, self._model.action_count - 1)
-            else:
-                return np.argmax(self._model.predict_one(state, self._sess))
-        else:
-            return random.randint(0, self._model.action_count - 1)
-        
-
-    def _preproc(self, state):
-        state = state[35:185] # crop - remove 35px from start & 25px from end of image in x, to reduce redundant parts of image (i.e. after ball passes paddle)
-        state = state[::2,::2,0] # downsample by factor of 2.
-        state[state == 144] = 0 # erase background (background type 1)
-        state[state == 109] = 0 # erase background (background type 2)
-        state[state != 0] = 1 # everything else (paddles, ball) just set to 1. this makes the image grayscale effectively
-        return state.astype(np.float).ravel()
-
-        
+    #check if hits the other side
+    if (
+                        ballXPos >= WINDOW_WIDTH - PADDLE_WIDTH - PADDLE_BUFFER and ballYPos + BALL_HEIGHT >= paddle2YPos and ballYPos - BALL_HEIGHT <= paddle2YPos + PADDLE_HEIGHT):
+        #switch directions
+        ballXDirection = -1
+    #past it
+    elif (ballXPos >= WINDOW_WIDTH - BALL_WIDTH):
+        #positive score
+        ballXDirection = -1
+        score = 1
+        return [score, paddle1YPos, paddle2YPos, ballXPos, ballYPos, ballXDirection, ballYDirection]
     
-    def _replay(self, states, next_states):
-        # states = []
-        # next_states = []
-        # batch = self._memory.sample(self._model.batch_size)
-        # states.append([val[0] for val in batch])
-        # next_states.append([np.zeros(self._model.state_count) if val[3] is None else val[3] for val in batch])
-        
-        q_value = self._model.predict_batch(states, self._sess)
-        q_value_pred = self._model.predict_batch(next_states, self._sess)
-        x = np.zeros((len(batch), self._model.state_count))
-        y = np.zeros((len(batch), self._model.action_count))
-        for i, b in enumerate(batch):
-            state, action, reward, next_state = b[0], b[1], b[2], b[3]
-            current_q_value = q_value[i]
-            if next_state is None:
-                current_q_value[action] = reward
-            else:
-                current_q_value[action] = reward + GAMMA * np.amax(q_value_pred[i])
-                # print("Q Value Predicted: ", q_value_pred[i])
-                # print("Current Q Value Action: ", current_q_value[action])
-            
-            x[i] = np.reshape(state, (1, self._model.state_count))
-            y[i] = current_q_value
-        
-        self._model.train_batch(self._sess, x, y)
+    #if it hits the top
+    #move down
+    if (ballYPos <= 0):
+        ballYPos = 0;
+        ballYDirection = 1;
+    #if it hits the bottom, move up
+    elif (ballYPos >= WINDOW_HEIGHT - BALL_HEIGHT):
+        ballYPos = WINDOW_HEIGHT - BALL_HEIGHT
+        ballYDirection = -1
+    return [score, paddle1YPos, paddle2YPos, ballXPos, ballYPos, ballXDirection, ballYDirection]
 
-        @property
-        def stored_rewards(self):
-            return self._stored_rewards
-        
-        @property
-        def stored_max_x(self):
-            return self._stored_max_x
+#update the paddle position
+def updatePaddle1(action, paddle1YPos):
+    #if move up
+    if (action[1] == 1):
+        paddle1YPos = paddle1YPos - PADDLE_SPEED
+    #if move down
+    if (action[2] == 1):
+        paddle1YPos = paddle1YPos + PADDLE_SPEED
+
+    #don't let it move off the screen
+    if (paddle1YPos < 0):
+        paddle1YPos = 0
+    if (paddle1YPos > WINDOW_HEIGHT - PADDLE_HEIGHT):
+        paddle1YPos = WINDOW_HEIGHT - PADDLE_HEIGHT
+    return paddle1YPos
 
 
+def updatePaddle2(paddle2YPos, ballYPos):
+    #move down if ball is in upper half
+    if (paddle2YPos + PADDLE_HEIGHT/2 < ballYPos + BALL_HEIGHT/2):
+        paddle2YPos = paddle2YPos + PADDLE_SPEED
+    #move up if ball is in lower half
+    if (paddle2YPos + PADDLE_HEIGHT/2 > ballYPos + BALL_HEIGHT/2):
+        paddle2YPos = paddle2YPos - PADDLE_SPEED
+    #don't let it hit top
+    if (paddle2YPos < 0):
+        paddle2YPos = 0
+    #dont let it hit bottom
+    if (paddle2YPos > WINDOW_HEIGHT - PADDLE_HEIGHT):
+        paddle2YPos = WINDOW_HEIGHT - PADDLE_HEIGHT
+    return paddle2YPos
 
-if __name__=="__main__":
-    env_name = "PongNoFrameskip-v4"
-    env = gym.make(env_name)
-    num_states = (75*80)
-    num_actions = env.action_space.n
 
-    model = model.Model(num_states, num_actions, BATCH_SIZE)
-    mem = memory.Memory(50000)
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
-        sess.run(model.var_init)
-        game = Game(sess, model, env, mem, MAX_EPSILON, MIN_EPSILON, LAMBDA)
-        
-        num_episodes = 1000
-        count = 0
-        while count < num_episodes:
-            if count % 10 == 0:
-                print('Episode {} of {}'.format(count+1, num_episodes))
-            game.run()
-            count += 1
-        save_path = saver.save(sess, "/tmp/model.ckpt")
-        print("Model saved in path: %s" % save_path)
-        # plt.plot(game.reward_store)
-        # plt.show()
-        # plt.close("all")
-        # plt.plot(game.max_x_store)
-        # plt.show()
+#game class
+class PongGame:
+    def __init__(self):
+        #random number for initial direction of ball
+        num = random.randint(0,9)
+        #keep score
+        self.tally = 0
+        #initialie positions of paddle
+        self.paddle1YPos = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2
+        self.paddle2YPos = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2
+        #and ball direction
+        self.ballXDirection = 1
+        self.ballYDirection = 1
+        #starting point
+        self.ballXPos = WINDOW_WIDTH/2 - BALL_WIDTH/2
+
+        #randomly decide where the ball will move
+        if(0 < num < 3):
+            self.ballXDirection = 1
+            self.ballYDirection = 1
+        if (3 <= num < 5):
+            self.ballXDirection = -1
+            self.ballYDirection = 1
+        if (5 <= num < 8):
+            self.ballXDirection = 1
+            self.ballYDirection = -1
+        if (8 <= num < 10):
+            self.ballXDirection = -1
+            self.ballYDirection = -1
+        #new random number
+        num = random.randint(0,9)
+        #where it will start, y part
+        self.ballYPos = num*(WINDOW_HEIGHT - BALL_HEIGHT)/9
+
+    #
+    def getPresentFrame(self):
+        #for each frame, calls the event queue, like if the main window needs to be repainted
+        pygame.event.pump()
+        #make the background black
+        screen.fill(BLACK)
+        #draw our paddles
+        drawPaddle1(self.paddle1YPos)
+        drawPaddle2(self.paddle2YPos)
+        #draw our ball
+        drawBall(self.ballXPos, self.ballYPos)
+        #copies the pixels from our surface to a 3D array. we'll use this for RL
+        image_data = pygame.surfarray.array3d(pygame.display.get_surface())
+        #updates the window
+        pygame.display.flip()
+        #return our surface data
+        return image_data
+
+    #update our screen
+    def getNextFrame(self, action):
+        pygame.event.pump()
+        score = 0
+        screen.fill(BLACK)
+        #update our paddle
+        self.paddle1YPos = updatePaddle1(action, self.paddle1YPos)
+        drawPaddle1(self.paddle1YPos)
+        #update evil AI paddle
+        self.paddle2YPos = updatePaddle2(self.paddle2YPos, self.ballYPos)
+        drawPaddle2(self.paddle2YPos)
+        #update our vars by updating ball position
+        [score, self.paddle1YPos, self.paddle2YPos, self.ballXPos, self.ballYPos, self.ballXDirection, self.ballYDirection] = updateBall(self.paddle1YPos, self.paddle2YPos, self.ballXPos, self.ballYPos, self.ballXDirection, self.ballYDirection)
+        #draw the ball
+        drawBall(self.ballXPos, self.ballYPos)
+        #get the surface data
+        image_data = pygame.surfarray.array3d(pygame.display.get_surface())
+        #update the window
+        pygame.display.flip()
+        #record the total score
+        self.tally = self.tally + score
+        print("Tally is " + str(self.tally))
+        #return the score and the surface data
+        return [score, image_data]
