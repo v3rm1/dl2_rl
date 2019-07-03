e597fd6f282a5ae0c057630b89bde95373e03ebf
Varun Ravi Varma <varunravivarma@gmail.com>
2019-07-03 14:12:25 +0200

    adding bash cmd for srvr

---
+++ ppo_confidence/dl2_ppo_conf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=70:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=dl2_ppo_conf_pong
+#SBATCH --mem=72000
+module load Python/3.6.4-foss-2018a
+module load CUDA/9.1.85
+module load Boost/1.66.0-foss-2018a-Python-3.6.4
+module load TensorFlow/1.12.0-foss-2018a-Python-3.6.4
+#pip install --user pygame
+#pip install --user keras
+#pip install --user matplotlib
+python ./ppo_conf/main.py
+mv *.out slurm/
